3b76500935909529ffe9c013a904da96b74df736
Daniel Belz <daniel_belz@gmx.net>
2019-11-24 00:18:31 +0100

    Removed old sample code

--- samples/NeatInput.ConsoleSample/Program.cs
+++ samples/NeatInput.ConsoleSample/Program.cs
@@ -1,7 +1,4 @@
-﻿using NeatInput.Domain.Processing.Keyboard;
-using NeatInput.Domain.Processing.Mouse;
-using NeatInput.Windows.Native.Window;
-using System;
+﻿using System;
 
 namespace NeatInput.ConsoleSample
 {
@@ -9,30 +6,9 @@ namespace NeatInput.ConsoleSample
     {
         private static void Main()
         {
-            var x = new NativeWindow(WndProc);
-            x.StartMessageLoop();
-
-            //var inputProvider = new InputProvider();
-            //inputProvider.KeyboardInputReceived += OnKeyboardInputReceived;
-            //inputProvider.MouseInputReceived += OnMouseInputReceived;
+            var source = new InputSource();
 
             Console.ReadLine();
-        }
-
-        // todo wrap
-        public static void WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
-        {
-
-        }
-
-        private static void OnMouseInputReceived(MouseInput input)
-        {
-            Console.WriteLine($"MOUSE => Key: {input.Key}, State: {input.State}, X: {input.X}, Y: {input.Y}, Simulated: {input.WasSimulated}");
-        }
-
-        private static void OnKeyboardInputReceived(KeyboardInput input)
-        {
-            Console.WriteLine($"KEYBOARD => Key: {input.Key}, State: {input.State}, Simulated: {input.WasSimulated}");
-        }
+        }        
     }
 }
