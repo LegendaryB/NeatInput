da106b8c3f4e075a956902f169bc153a9140e0b3
Daniel Belz <daniel_belz@gmx.net>
2019-12-30 02:56:56 +0100

    Removed AppDomain event subscriptions because it is the task of the application to unload all used components on app exit

--- src/NeatInput/InputSource.cs
+++ src/NeatInput/InputSource.cs
@@ -14,28 +14,18 @@ namespace NeatInput
         public InputSource()
         {
             _instance = GetPlatformImplementation();
-
-            AppDomain.CurrentDomain.ProcessExit += OnAppDomainLifetimeEnded;
-            AppDomain.CurrentDomain.UnhandledException += OnAppDomainLifetimeEnded;
         }
 
         public void Dispose()
         {
             _instance.Dispose();
-
-            AppDomain.CurrentDomain.ProcessExit -= OnAppDomainLifetimeEnded;
-            AppDomain.CurrentDomain.UnhandledException -= OnAppDomainLifetimeEnded;
         }
         private IInputSource GetPlatformImplementation()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return new WindowsInputSource();
-            //else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            //    return new LinuxInputSource();
             else
                 throw new InvalidOperationException("This library is only available for windows systems!");
         }
-
-        private void OnAppDomainLifetimeEnded(object s, object e) => Dispose();
     }
 }
