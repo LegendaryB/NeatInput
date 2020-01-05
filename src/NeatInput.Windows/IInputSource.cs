400b7bf01ba9f139eed24dbe28e860d93a1ca759
Daniel Belz <daniel_belz@gmx.net>
2020-01-05 14:16:42 +0100

    Added documentation for InputSource and the interface

--- src/NeatInput.Windows/IInputSource.cs
+++ src/NeatInput.Windows/IInputSource.cs
@@ -2,8 +2,15 @@
 
 namespace NeatInput.Windows
 {
+    /// <summary>
+    /// This is the interface for the <see cref="InputSource"/> class which manages the mouse and keyboard hook internally and populates the results
+    /// back to the user provided receivers.
+    /// </summary>
     public interface IInputSource : IDisposable
     {
+        /// <summary>
+        /// This method installs the mouse and/or the keyboard hook and starts listening for input events.
+        /// </summary>
         void Listen();
     }
 }
