d2eccc4d9ce5e556372499974054bfaaea6d4b3d
Daniel Belz <daniel_belz@gmx.net>
2020-01-08 02:46:29 +0100

    Added missing property to struct

--- src/NeatInput.Windows/Interop/Interop.KBDLLHOOKSTRUCT.cs
+++ src/NeatInput.Windows/Interop/Interop.KBDLLHOOKSTRUCT.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 internal enum KBDLLHOOKSTRUCTFlags : uint
 {
     LLKHF_EXTENDED = 0x01,
+    LLKHF_LOWER_IL_INJECTED = 0x02,
     LLKHF_INJECTED = 0x10,
     LLKHF_ALTDOWN = 0x20,
     LLKHF_UP = 0x80
